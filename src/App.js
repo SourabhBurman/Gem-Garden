5895f2d2cdd2873efd5d9ce7ba2be141157697aa
sachinmohite <sachinmohite708@gmail.com>
2024-01-17 11:57:49 +0530

    footerpart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
-     
+     <Footer/>
     </div>
   );
 }
